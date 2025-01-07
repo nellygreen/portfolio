c0212f9d9506d66dfe214fc77acf881790041a1b
nellygreen <nellygreen.cg@gmail.com>
2025-01-07 20:47:50 +0100

    added navbar

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="bg-[#111827] min-h-screen">
+        <NavBar />
+    </div>
+</template>
+
+<script setup>
+import { defineAsyncComponent } from 'vue';
+//import components asyncronously
+const NavBar = defineAsyncComponent(()=>import('@/components/NavBar.vue'));
+</script>
