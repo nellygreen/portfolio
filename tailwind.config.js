1372aa0d127a33611cf95807a1c547ab9142349b
nellygreen <nellygreen.cg@gmail.com>
2025-02-03 18:29:59 +0100

    hero section

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#f59e0b',
+        secondary:'#ec4899'
+      }
+    },
+  },
+  plugins: [],
+}
+
