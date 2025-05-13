52bc0ebcddd2b29002f39f39d7de68fcbed4909b
nellygreen <nellygreen.cg@gmail.com>
2025-05-13 14:49:04 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/portfolio",
   resolve:{
     alias:{
       '@':fileURLToPath(new URL("./src", import.meta.url))
